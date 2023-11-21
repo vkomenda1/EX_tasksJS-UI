43f3300ce643f04bc58ad3a72f52944272933f70
vkomenda1 <wkomenda@gmail.com>
2023-11-21 14:55:44 +0200

    added screen extension code, page reload and basic url

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
+      baseUrl: 'https://juice-shop-sanitarskyi.herokuapp.com/'
       // implement node event listeners here
     },
   },
